575e7cfe51f832581f60419b4003375c9cf25192
Chanalizain <inchanaliza@gmail.com>
2026-01-03 15:02:41 +0700

    add user model

--- sleep_blocker/lib/models/sleep_log.dart
+++ sleep_blocker/lib/models/sleep_log.dart
@@ -1,10 +1,12 @@
 class SleepLog {
+  final String userId;
   final String id; 
   final DateTime date;
   final double duration; 
   final int qualityScore; 
 
   SleepLog({
+    required this.userId,
     required this.id,
     required this.date,
     required this.duration,
